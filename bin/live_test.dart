14846420bc14f86a80a9538e9f715032f0d95403
Jahirul Islam <jahiruls2020@gmail.com>
2026-03-28 17:35:51 +0600

    Initial commit

---
+++ bin/live_test.dart
@@ -0,0 +1,27 @@
+class Car {
+  // Public properties
+  String brand;
+  String model;
+  int year;
+
+  // Constructor
+  Car(this.brand, this.model, this.year);
+
+  // Method to calculate car age
+  int carAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
+
+void main() {
+  // Create an instance of Car
+  Car myCar = Car("Toyota", "Corolla", 2015);
+
+  // Display information
+  print("Brand: ${myCar.brand}");
+  print("Model: ${myCar.model}");
+  print("Year: ${myCar.year}");
+  print("Car Age: ${myCar.carAge()} years");
+}
+
